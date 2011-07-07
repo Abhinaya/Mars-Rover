ee5252a185a3fb9369dd02f64734212a461dec58
Abhinaya <abhijan90@gmail.com>
2011-07-08 00:00:02 +0530

    added parse function

---
+++ Mars-Rover1/src/main/RoverPosition.java
@@ -0,0 +1,32 @@
+package main;
+
+public class RoverPosition {
+
+    private int x;
+    private int y;
+    private String direction;
+
+    private RoverPosition(){
+    }
+
+    public static RoverPosition parse(String input) {
+        String[] position= input.split(" ");
+        RoverPosition roverPosition = new RoverPosition();
+        roverPosition.x= Integer.parseInt(position[0]);
+        roverPosition.y= Integer.parseInt(position[1]);
+        roverPosition.direction=position[2];
+        return roverPosition;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
--- Mars-Rover1/src/test/RoverPositionTest.java
+++ Mars-Rover1/src/test/RoverPositionTest.java
@@ -1,12 +1,18 @@
 package test;
 
 
+import main.RoverPosition;
 import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
 public class RoverPositionTest {
     @Test
-    public void shouldblahblah(){
-
+    public void shouldParseInput(){
+        RoverPosition roverPosition= RoverPosition.parse("2 1 N");
+        assertThat(roverPosition.getX(),is(2));
+        assertThat(roverPosition.getY(),is(1));
+        assertThat(roverPosition.getDirection(),is("N"));
     }
-
 }
