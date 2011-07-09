8305a664118fa4b3f968c3187e230f33d22069cd
Abhinaya <abhijan90@gmail.com>
2011-07-09 15:03:25 +0530

    Added tests and cases for rover move when facing north and east

--- Mars-Rover1/src/main/RoverPosition.java
+++ Mars-Rover1/src/main/RoverPosition.java
@@ -4,9 +4,9 @@ public class RoverPosition {
 
     private int x;
     private int y;
-    private String direction;
+    private char direction;
 
-    private RoverPosition(){
+    RoverPosition(){
     }
 
     public static RoverPosition parse(String input) {
@@ -14,7 +14,7 @@ public class RoverPosition {
         RoverPosition roverPosition = new RoverPosition();
         roverPosition.x= Integer.parseInt(position[0]);
         roverPosition.y= Integer.parseInt(position[1]);
-        roverPosition.direction=position[2];
+        roverPosition.direction=position[2].charAt(0);
         return roverPosition;
     }
 
@@ -26,16 +26,40 @@ public class RoverPosition {
         return y;
     }
 
-    public String getDirection() {
+    public char getDirection() {
         return direction;
     }
 
     public void move() {
+        switch (direction){
+            case 'N':
+                this.y+=1;
+                break;
+            case 'E':
+                this.x+=1;
+                break;
+
+        }
     }
 
     public void turnRight() {
+        switch (direction){
+            case 'N':
+                direction='E';
+                break;
+            case 'E':
+                direction='S';
+                break;
+            case 's':
+                direction='W';
+                break;
+            case 'W':
+                direction='N';
+                break;
+        }
     }
 
     public void turnLeft() {
+
     }
 }
--- Mars-Rover1/src/test/RoverPositionTest.java
+++ Mars-Rover1/src/test/RoverPositionTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class RoverPositionTest {
     @Test
@@ -13,6 +14,24 @@ public class RoverPositionTest {
         RoverPosition roverPosition= RoverPosition.parse("2 1 N");
         assertThat(roverPosition.getX(),is(2));
         assertThat(roverPosition.getY(), is(1));
-        assertThat(roverPosition.getDirection(),is("N"));
+        assertThat(roverPosition.getDirection(),is('N'));
+    }
+
+    @Test
+    public void shouldMoveWhenFacingNorth(){
+        RoverPosition roverPosition=RoverPosition.parse("1 2 N");
+        roverPosition.move();
+        assertThat(roverPosition.getDirection(), is('N'));
+        assertThat(roverPosition.getX(), is(1));
+        assertThat(roverPosition.getY(), is(3));
+        }
+
+    @Test
+    public void shouldMoveWhenFacingEast(){
+        RoverPosition roverPosition=RoverPosition.parse("1 2 E");
+        roverPosition.move();
+        assertThat(roverPosition.getDirection(),is('E'));
+        assertThat(roverPosition.getX(),is(2));
+        assertThat(roverPosition.getY(),is(2));
     }
 }
